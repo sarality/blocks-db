fb7e4bb828f9cf3c91d120c9ec89484e8af65a69
Abhideep Singh <abhi@dothat.in>
2016-05-25 11:49:05 +0530

    Class to build a simple query for a database call.

---
+++ src/main/java/com/sarality/db/query/LogicalOperator.java
@@ -0,0 +1,12 @@
+package com.sarality.db.query;
+
+/**
+ * Enum for Logical Operators used in a Query
+ *
+ * @author abhideep@ (Abhideep Singh)
+ */
+public enum LogicalOperator {
+  AND,
+  OR,
+  NOT
+}
